420cb15b9075a65a48d5eb6273830fb778a59b3e
Wellington <wrcnegao@gomail.com>
2025-10-28 15:30:05 -0300

    IOT  - DHT11

--- index.js
+++ index.js
@@ -2,26 +2,24 @@ const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
 const mqtt = require("mqtt");
 
-// Porta do Arquivo (ajuste "COM3" no Windows ou "/dev/ttyUSB0" no Linux)
-const port = new SerialPort({ path: "COM3", baudRate: 9600 });
+const port = new SerialPort ({ path: "COM3", baudRate: 9600 });
 const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
 
-// Conecxão ao Brocker MQTT público HiveMQ
 const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
-const topic = "senai/Wellin";
+const topic = "senai/iot/dht11";
 
 client.on("connect", () => {
-  console.log("Conectado ao broker MQTT!");
+    console.log("Conectado ao broker MQTT!");
 });
 
 parser.on("data", (line) => {
-  try {
-    const data = JSON.parse(line.trim());
-    console.log("Recebido:", data);
+    try{
+        const data = JSON.parse(line.trim());
+        console.log("Recebido:", data);
 
-    client.publish(topic, JSON.stringify(data));
-    console.log("Publicado no MQTT:", data);
-  } catch (err) {
-    console.error("Erro ao parsear:", line);
-  }
+        client.publish(topic, JSON.stringify(data));
+        console.log("Publicado no MQTT:", data);
+    } catch(err) {
+        console.log("Erro ao parser:", line);
+    }
 });
